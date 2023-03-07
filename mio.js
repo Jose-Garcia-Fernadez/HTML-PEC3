910be2ea1554bd302a8ab35988a76d28af4617fc
Jose-Garcia-Fernadez <81307873+Jose-Garcia-Fernadez@users.noreply.github.com>
2023-03-07 16:59:51 +0100

    Update mio.js

--- mio.js
+++ mio.js
@@ -1,5 +1,15 @@
-window.onload = function () {document.onmousemove = mostrarPosicionPuntero;		document.onclick=copiarportapapeles;} 
-function mostrarPosicionPuntero(e) {document.title =((document.all)? event.x : e.pageX) + ' ' + document.documentElement.clientWidth + "  " +
+ window.onload = function () {document.onmousemove = mostrarPosicionPuntero;		document.onclick=copiarportapapeles;} 
+  function mostrarPosicionPuntero(e) {document.title =((document.all)? event.x : e.pageX) + ' ' + document.documentElement.clientWidth + "  " +
    ((document.all)? event.y : e.pageY) +  ' '+ document.documentElement.clientHeight;   }
-function copiarportapapeles(e){var str=((document.all)? event.x : e.pageX) + '\n' + ((document.all)? event.y : e.pageY);    
-      const el = document.createElement('textarea');el.value = str;document.body.appendChild(el);el.select();document.execCommand('copy');document.body.removeChild(el);}    
+  
+    function copiarportapapeles(e){
+      var str=(  (document.all)? event.x : e.pageX) + '\n' + ((document.all)? event.y : e.pageY);   
+      str= ((document.all)? event.x : e.pageX) + ' ' + document.documentElement.clientWidth + "  " +
+      ((document.all)? event.y : e.pageY) +  ' '+ document.documentElement.clientHeight;
+      
+      const el = document.createElement('textarea');
+      el.value = str;
+      document.body.appendChild(el);
+      el.select();
+      document.execCommand('copy');
+      document.body.removeChild(el);}    
