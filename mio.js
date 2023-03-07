2caaa32cb9823806558cb0da842250fad409f550
Jose-Garcia-Fernadez <81307873+Jose-Garcia-Fernadez@users.noreply.github.com>
2023-03-07 17:08:45 +0100

    Update mio.js

--- mio.js
+++ mio.js
@@ -1,20 +1,17 @@
- window.onload = function () {document.onmousemove = mostrarPosicionPuntero;		document.onclick=copiarportapapeles;} 
+window.onload = function () {document.onmousemove = mostrarPosicionPuntero;		document.onclick=copiarportapapeles;} 
   function mostrarPosicionPuntero(e) {document.title =((document.all)? event.x : e.pageX) + ' ' + document.documentElement.clientWidth + "  " +
    ((document.all)? event.y : e.pageY) +  ' '+ document.documentElement.clientHeight;   }
   
     function copiarportapapeles(e){
       var str=(  (document.all)? event.x : e.pageX) + '\n' + ((document.all)? event.y : e.pageY);   
-      
-     
-     str= ((document.all)? event.x : e.pageX) + ' ' + document.documentElement.clientWidth + "  " +
+      str= ((document.all)? event.x : e.pageX) + ' ' + document.documentElement.clientWidth + "  " +
       ((document.all)? event.y : e.pageY) +  ' '+ document.documentElement.clientHeight;
       
       const el = document.createElement('textarea');
       el.value = str;
-     el.value ="202";
       document.body.appendChild(el);
       el.select();
       document.execCommand('copy');
       document.body.removeChild(el);
-    alert(str);
-    }    
+	alert(str);
+	}    
